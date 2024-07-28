5b2d9a6c0924e08e3927b0143ec8c1eb028aab70
nur farazi <nur369188@gmail.com>
2024-07-29 00:14:01 +0600

    chore: Update .gitignore and Program.cs

--- ThreadingPlay/Program.cs
+++ ThreadingPlay/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Main thread started");
+        await DownloadTask1();
+        await Task.WhenAll(DownloadTask2(), DownloadTask3(), DownloadTask1());
+        await Task.WhenAny(DownloadTask2(), DownloadTask3(), DownloadTask1());
+        Console.WriteLine("Main thread completed");
+    }
+
+    private static Task DownloadTask1()
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine("DownloadTask1 completed");
+        return Task.CompletedTask;
+    }
+    
+    private static Task DownloadTask2()
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine("DownloadTask2 completed");
+        return Task.CompletedTask;
+    }
+    
+    private static Task DownloadTask3()
+    {
+        Thread.Sleep(5000);
+        Console.WriteLine("DownloadTask3 completed");
+        return Task.CompletedTask;
+    }
+}
+
